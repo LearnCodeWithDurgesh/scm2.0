25f784dad28c7e624510d9ad71a25045c13f769f
Learn Code With Durgesh Officials <55891143+LearnCodeWithDurgesh@users.noreply.github.com>
2024-04-27 15:14:22 +0530

    understanding fragments

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -10,11 +10,28 @@ public class PageController {
     @RequestMapping("/home")
     public String home(Model model) {
         System.out.println("Home page handler");
-
         // sending data to view
         model.addAttribute("name", "Substring Technologies");
         model.addAttribute("youtubeChannel", "Learn Code With Durgesh");
         model.addAttribute("githubRepo", "https://github.com/learncodewithdurgesh/");
         return "home";
     }
+
+    // about route
+
+    @RequestMapping("/about")
+    public String aboutPage(Model model) {
+        model.addAttribute("isLogin", true);
+        System.out.println("About page loading");
+        return "about";
+    }
+
+    // services
+
+    @RequestMapping("/services")
+    public String servicesPage() {
+        System.out.println("services page loading");
+        return "services";
+    }
+
 }
